71ea2b29ad92d1e917e1e8567ef62677cdafc862
Karel Břinda <karel.brinda@gmail.com>
2018-11-12 14:06:35 -0500

    Avoid repetitive redownloading (2)

--- codeocean/_prepare_code.sh
+++ codeocean/_prepare_code.sh
@@ -7,12 +7,12 @@ set -u
 rm -fr _code
 mkdir _code
 
-make -C ../01* clean
-make -C ../02* clean
-make -C ../03* clean
-make -C ../04* clean
-
 cp -av ../??_* _code
+
+make -C _code/01_* clean
+make -C _code/02_* clean
+make -C _code/03_* clean
+make -C _code/04_* clean
 
 cp run.sh ../LICENSE README.md _code
 
